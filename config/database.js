1e9ddae23db35e616b8bc085e3a68b631c0486e1
beola134 <nguyentantai612004@gmail.com>
2024-12-10 19:36:35 +0700

    fix

--- config/database.js
+++ config/database.js
@@ -1,4 +1,5 @@
 const { Sequelize } = require('sequelize');
+
 const sequelize = new Sequelize('btbemsqj96zftsigmco2', 'ukqlr76ulzopwjd3', 'VXV8Uth9yjdLoOcQlyEG', {
   host: 'btbemsqj96zftsigmco2-mysql.services.clever-cloud.com',
   port: 3306,
@@ -10,4 +11,5 @@ const sequelize = new Sequelize('btbemsqj96zftsigmco2', 'ukqlr76ulzopwjd3', 'VXV
     },
   },
 });
+
 module.exports = sequelize;
