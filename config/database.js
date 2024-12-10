5fe85452892a6b322beba49258e0cb091ed98874
beola134 <nguyentantai612004@gmail.com>
2024-12-10 16:51:29 +0700

    fix

---
+++ config/database.js
@@ -0,0 +1,16 @@
+const { Sequelize } = require('sequelize');
+
+const sequelize = new Sequelize('btbemsqj96zftsigmco2', 'ukqlr76ulzopwjd3', 'VXV8Uth9yjdLoOcQlyEG', {
+  host: 'btbemsqj96zftsigmco2-mysql.services.clever-cloud.com',
+  port: 3306,
+  dialect: 'mysql',
+  logging: false,
+  dialectOptions: {
+    ssl: {
+      require: true,
+      rejectUnauthorized: false,
+    },
+  },
+});
+
+module.exports = sequelize;
