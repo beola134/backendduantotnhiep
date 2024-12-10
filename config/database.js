29b7733b0edbd350c1e73a99d2a7eac1bf2d36dc
beola134 <nguyentantai612004@gmail.com>
2024-12-10 19:20:51 +0700

    fix

--- config/database.js
+++ config/database.js
@@ -3,7 +3,7 @@ const { Sequelize } = require('sequelize');
 const sequelize = new Sequelize('btbemsqj96zftsigmco2', 'ukqlr76ulzopwjd3', 'VXV8Uth9yjdLoOcQlyEG', {
   host: 'btbemsqj96zftsigmco2-mysql.services.clever-cloud.com',
   port: 3306,
-  dialect: 'mysql',
+  dialect: 'postgres', // Hoặc mysql
   logging: false,
   dialectOptions: {
     ssl: {
